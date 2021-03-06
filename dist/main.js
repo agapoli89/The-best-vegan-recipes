66405bdd8c05ab396833df46a691d84a1df6da4b
Agnieszka Polińska <agapoli@gmail.com>
2021-03-06 15:11:46 +0100

    Change to the production version

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,4 @@
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 var Title = function Title() {
   return React.createElement(
@@ -14,70 +8,81 @@ var Title = function Title() {
   );
 };
 
-var Post = function (_React$Component) {
-  _inherits(Post, _React$Component);
-
-  function Post(props) {
-    _classCallCheck(this, Post);
-
-    var _this = _possibleConstructorReturn(this, (Post.__proto__ || Object.getPrototypeOf(Post)).call(this, props));
-
-    _this.state = {
-      isAllContentShown: false
+// VERSION WITH CLASS COMPONENT - COMMENTED
+/* class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAllContentShown: false,
     };
-    return _this;
   }
 
-  _createClass(Post, [{
-    key: "toggleContent",
-    value: function toggleContent() {
-      this.setState({ isAllContentShown: !this.state.isAllContentShown });
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        null,
-        React.createElement(
-          "h2",
-          null,
-          this.props.title
-        ),
-        this.state.isAllContentShown ? React.createElement(
-          "div",
-          null,
-          React.createElement(
-            "p",
-            null,
-            this.props.content
-          ),
-          React.createElement(
-            "button",
-            { onClick: this.toggleContent.bind(this) },
-            "Poka\u017C mniej"
-          )
-        ) : React.createElement(
-          "div",
-          null,
-          React.createElement(
-            "p",
-            null,
-            this.props.shortContent
-          ),
-          React.createElement(
-            "button",
-            { onClick: this.toggleContent.bind(this) },
-            "Poka\u017C ca\u0142y przepis"
-          )
-        ),
-        React.createElement("hr", null)
-      );
-    }
-  }]);
+  toggleContent() {
+    this.setState({isAllContentShown: !this.state.isAllContentShown});
+  }
 
-  return Post;
-}(React.Component);
+  render() {
+    return (
+      <div>
+        <h2>{this.props.title}</h2>
+        {this.state.isAllContentShown 
+        ? <div><p>{this.props.content}</p><button onClick={this.toggleContent.bind(this)}>Pokaż mniej</button></div> 
+        : <div><p>{this.props.shortContent}</p><button onClick={this.toggleContent.bind(this)}>Pokaż cały przepis</button></div>}
+        <hr />
+      </div>
+  );
+  }
+} */
+
+//VERSION WITH FUNCTION COMPONENT
+var Post = function Post(props) {
+  var _React$useState = React.useState(false),
+      _React$useState2 = _slicedToArray(_React$useState, 2),
+      isAllContentShown = _React$useState2[0],
+      toggleContent = _React$useState2[1];
+
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h2",
+      null,
+      props.title
+    ),
+    isAllContentShown ? React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "p",
+        null,
+        props.content
+      ),
+      React.createElement(
+        "button",
+        { onClick: function onClick() {
+            return toggleContent(!isAllContentShown);
+          } },
+        "Poka\u017C mniej"
+      )
+    ) : React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "p",
+        null,
+        props.shortContent
+      ),
+      React.createElement(
+        "button",
+        { onClick: function onClick() {
+            return toggleContent(!isAllContentShown);
+          } },
+        "Poka\u017C ca\u0142y przepis"
+      )
+    ),
+    React.createElement("hr", null)
+  );
+};
 
 var Blog = function Blog() {
   var posts = [{
